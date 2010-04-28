146ab60870e93ff8361f4326fb13cabeb350e3b5
Matthijs Groen <matthijs@matthijs-desktop.(none)>
2010-04-28 16:37:25 +0200

    Tweede versie

--- lib/result_parser.rb
+++ lib/result_parser.rb
@@ -1,9 +1,49 @@
+require 'rubygems'
+require 'fastercsv'
 
 class ResultParser
 
 	def initialize scoring_filename, results_filename
 		@scoring_configuration = ScoringConfiguration.new scoring_filename
+		@results_filename = results_filename
+		@results = []
+	end
+
+	# Go through the file row by row, and calculate the scores for each row
+	def parse_results
+		@results = []
 		
+		result_rows = FasterCSV.read @results_filename
+		start_row = 1
+
+		# Kolom lijst:
+		# "matrixvak",
+		# "score in matrixvak",
+		# "indicator",
+		# "codering",
+		# "score in indicator",
+		# "vraag",
+		# "antwoordmogelijkheid",
+		# Score 1 - 16
+		start_row.times do data.shift end
+		data.each do |row|
+			matrix_tile, score_tile, indicator, question_id, indicator_score, question,
+				answers = row[0..6]
+			answer_scores = row[7..23]
+
+			# TODO detect question type and convert percentage columns
+
+			@scoring_map[question_id] = {
+				:matrix_tile => matrix_tile,
+				:score_tile => score_tile,
+				:indicator => indicator,
+				:indicator_score => indicator_score,
+				:question => question,
+				:answers => answers,
+				:answer_scoring => answer_scores
+			}
+		end
+
 	end
 
 end
--- lib/scoring_configuration.rb
+++ lib/scoring_configuration.rb
@@ -1,9 +1,43 @@
+require 'rubygems'
+require 'fastercsv'
+
 class ScoringConfiguration
 
 	def initialize configuration_filename
-		
+		data = FasterCSV.read configuration_filename
+		start_row = 1
 
+		@scoring_map = {}
+
+		# Kolom lijst:
+		# "matrixvak",
+		# "score in matrixvak",
+		# "indicator",
+		# "codering",
+		# "score in indicator",
+		# "vraag",
+		# "antwoordmogelijkheid",
+		# Score 1 - 16
+		start_row.times do data.shift end
+		data.each do |row|
+			matrix_tile, score_tile, indicator, question_id, indicator_score, question,
+				answers = row[0..6]
+			answer_scores = row[7..23]
+
+			# TODO detect question type and convert percentage columns			
+
+			@scoring_map[question_id] = {
+				:matrix_tile => matrix_tile,
+				:score_tile => score_tile,
+				:indicator => indicator,
+				:indicator_score => indicator_score,
+				:question => question,
+				:answers => answers,
+				:answer_scoring => answer_scores			
+			}
+		end
 
 	end
 
 end
+
